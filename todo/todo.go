401c7978dcc2c1cafc051f1ee66d31f0c54c892b
mass8 <29457120+h64m1@users.noreply.github.com>
2021-03-26 21:36:06 +0900

    escape vertical bar

--- todo/todo.go
+++ todo/todo.go
@@ -10,3 +10,23 @@ type Todo struct {
 type Panel struct {
 	Cell [9]string
 }
+
+// EscapeCell escape vertical bar "|"
+func (panel Panel) EscapeCell() [9]string {
+	var escapedCell [9]string
+	for i, cell := range panel.Cell {
+		b := make([]byte, 0, 16)
+		for _, s := range cell {
+			if s == 124 {
+				// escape vertical bar
+				b = append(b, "&#124;"...)
+			} else {
+				b = append(b, string(s)...)
+			}
+		}
+
+		escapedCell[i] = string(b)
+	}
+
+	return escapedCell
+}
