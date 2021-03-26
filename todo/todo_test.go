f5de6fb08330fc24afd6997173d1053dac8f5cee
mass8 <29457120+h64m1@users.noreply.github.com>
2021-03-26 22:41:37 +0900

    add test for todo.go

---
+++ todo/todo_test.go
@@ -0,0 +1,23 @@
+package todo
+
+import "testing"
+
+func TestEscapeCellSuccess(t *testing.T) {
+	grid := Todo{
+		Goal:  "",
+		Panel: [9]Panel{},
+	}
+	panel := grid.Panel[0]
+	panel.Cell[0] = "|"
+	panel.Cell[1] = "123456789"
+
+	result := panel.EscapeCell()
+
+	if result[0] != "&#124;" {
+		t.Fatal("failed test : result=", result)
+	}
+
+	if result[1] != "123456789" {
+		t.Fatal("failed test : result=", result)
+	}
+}
