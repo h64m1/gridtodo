79a498732a57b9c42e767bd2745a37206c9bb1fc
mass8 <29457120+h64m1@users.noreply.github.com>
2021-03-27 09:50:47 +0900

    use strings.Replace to escape special characters
    - vertical bar
    - line break: LF

--- todo/todo.go
+++ todo/todo.go
@@ -1,5 +1,9 @@
 package todo
 
+import (
+	"strings"
+)
+
 // 3*3 panel = (3*3 cell) * 9
 type Todo struct {
 	Goal  string
@@ -11,21 +15,22 @@ type Panel struct {
 	Cell [9]string
 }
 
+// Escape escape vertical line, line-break
+func Escape(text string) string {
+	// escape vertical bar
+	escaped := strings.Replace(text, "|", "&#124;", -1)
+
+	// convert "\n" to "<br>"
+	escaped = strings.Replace(escaped, "\n", "<br>", -1)
+
+	return escaped
+}
+
 // EscapeCell escape vertical bar "|"
 func (panel Panel) EscapeCell() [9]string {
 	var escapedCell [9]string
 	for i, cell := range panel.Cell {
-		b := make([]byte, 0, 16)
-		for _, s := range cell {
-			if s == 124 {
-				// escape vertical bar
-				b = append(b, "&#124;"...)
-			} else {
-				b = append(b, string(s)...)
-			}
-		}
-
-		escapedCell[i] = string(b)
+		escapedCell[i] = Escape(cell)
 	}
 
 	return escapedCell
